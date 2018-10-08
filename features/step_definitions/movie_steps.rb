e8e2a7438dd1e863e6fffb19d68d1d747e72ff7b
EC2 Default User <ec2-user@ip-172-31-30-48.us-east-2.compute.internal>
2018-10-08 01:42:01 +0000

    HW3

---
+++ features/step_definitions/movie_steps.rb
@@ -0,0 +1,12 @@
+
+
+Given /the following movies exist/ do |movies_table|
+    movies_table.hashes.each do |movie|
+        Movie.create!(movie)
+    end
+end
+
+Then /^the director of "(.*)" should be "(.*)"$/ do |title_value, director_value|
+    movie = Movie.find_by_title(title_value)
+    expect(movie.director).to eq director_value
+end
